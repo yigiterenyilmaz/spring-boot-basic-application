324e88f26b7f1aef73088086ee84591db2e9fa49
yigiterenyilmaz <yigiterenyilmaz05@gmail.com>
2026-01-09 11:39:36 +0300

    gitignore

--- spring-data-jpa/src/main/java/com/yigiteren/starter/aspects/LoggingAspect.java
+++ spring-data-jpa/src/main/java/com/yigiteren/starter/aspects/LoggingAspect.java
@@ -1,14 +1,22 @@
 package com.yigiteren.starter.aspects;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
 @Aspect
+@Slf4j
 public class LoggingAspect {
-    @Before("execution(* com.yigiteren.starter.services.StudentService.saveStudent(..))")
-    public void log(){
-        System.out.println("Student will be saved.");
+    @Around("execution(* com.yigiteren.starter.services.StudentService.saveStudent(..))")
+    public Object log(ProceedingJoinPoint pjp) throws Throwable{
+        log.info("Student is being registered");
+        Object result = pjp.proceed();
+        log.info("Student is registered.");
+        return result;
     }
 }
