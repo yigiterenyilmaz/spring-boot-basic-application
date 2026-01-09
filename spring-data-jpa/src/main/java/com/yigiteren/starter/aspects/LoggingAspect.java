5b46e4a19641d2caf9aea0412132fac4eaa884d9
yigiterenyilmaz <yigiterenyilmaz05@gmail.com>
2026-01-09 12:59:08 +0300

    @Aspect annotation ve Aspect logging classı eklendi

--- spring-data-jpa/src/main/java/com/yigiteren/starter/aspects/LoggingAspect.java
+++ spring-data-jpa/src/main/java/com/yigiteren/starter/aspects/LoggingAspect.java
@@ -4,6 +4,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +18,16 @@ public class LoggingAspect {
         log.info("Student is being registered");
         Object result = pjp.proceed();
         log.info("Student is registered.");
+        return result;
+    }
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    private void getLogging(){}
+
+    @Around("getLogging()")
+    public Object getLog(ProceedingJoinPoint pjp) throws Throwable{
+        log.info("Get method invoked.");
+        Object result = pjp.proceed();
+        log.info("Get method invocation terminated.");
         return result;
     }
 }
